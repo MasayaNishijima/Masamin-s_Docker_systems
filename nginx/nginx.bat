4b51a6ddd8857ac613db18da69442c52d1ad67e6
西嶋雅也 <masaya20000607@gmail.com>
2021-02-13 20:51:08 +0900

    起動時:相対パス
    起動時のhtmlフォルダ(index.htmlを置くところ)の指定を絶対パスから相対パスに変更

--- nginx/nginx.bat
+++ nginx/nginx.bat
@@ -13,7 +13,7 @@ if "%1"=="-s" (
     goto end
 ) else if "%1"=="-m" (
     echo "nginx make"
-    docker run --name nginx_1 -v "D:\Server\Masamin-s_Docker_systems\nginx\html":/usr/share/nginx/html:ro -d -p 8080:80 nginx
+    docker run --name nginx_1 -v "%CD%\html":/usr/share/nginx/html:ro -d -p 8080:80 nginx
     goto end
 ) else (
 echo "This bat file is needed char under here"
